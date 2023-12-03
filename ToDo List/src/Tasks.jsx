5d0005d9f9ca34463906543755585cbd3a2faba5
Manisangh Sharma <manisanghsharma@gmail.com>
2023-12-03 13:16:29 +0530

    Bug fixes

--- ToDo List/src/Tasks.jsx
+++ ToDo List/src/Tasks.jsx
@@ -4,10 +4,12 @@ import { MdDelete } from "react-icons/md";
 
 const Tasks = ({ tasks, setTasks, item }) => {
 
-  const [isClicked, setIsClicked] = useState(false);
+  // const [isClicked, setIsClicked] = useState(false);
 
-  const handleCheck = () => {
-    setIsClicked(!isClicked);
+  const handleCheck = (id) => {
+    // setIsClicked(!isClicked);
+    const updatedItems = tasks.map(item => (item.id == id ? {...item, checked: !item.checked} : item));
+    setTasks(updatedItems);
   }
 
   const handleDelete = (id) => {
@@ -19,23 +21,28 @@ const Tasks = ({ tasks, setTasks, item }) => {
 
 
   return (
-    <div>
-      <ul id="tasks-checklist">
-        <img src="Images/unchecked.png" className='checkIcon' onClick={handleCheck} />
-        {isClicked ? 
-        <li style={{textDecoration: "line-through"}}>
-          {item.task}
-        </li> 
-        :
-        (<li>{item.task}</li>)
-        }
-        
-        <MdDelete
-          style={{ cursor: 'pointer' }}
-          onClick={() => handleDelete(item.id)} />
-      </ul>
-    </div>
-  )
+		<div>
+			<ul id='tasks-checklist'>
+				<div onClick={() => handleCheck(item.id)}>
+					<img
+						src={item.checked ? "Images/checked.png" : "Images/unchecked.png"}
+						className='checkIcon'
+						// onClick={() => handleCheck(item.id)}
+					/>
+					{item.checked ? (
+						<li style={{ textDecoration: "line-through" }}>{item.task}</li>
+					) : (
+						<li>{item.task}</li>
+					)}
+				</div>
+
+				<MdDelete
+					style={{ cursor: "pointer" }}
+					onClick={() => handleDelete(item.id)}
+				/>
+			</ul>
+		</div>
+	);
 }
 
 export default Tasks
--- ToDo List/src/addTask.jsx
+++ ToDo List/src/addTask.jsx
@@ -1,64 +1,67 @@
-import React, { useState } from 'react';
-import { nanoid } from 'nanoid';
-import Tasks from './Tasks';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import Tasks from "./Tasks";
 const addTask = ({ tasks, setTasks }) => {
+	const [search, setSearch] = useState("");
 
-    const [search, setSearch] = useState('');
+	const addNewTask = () => {
+		const id = nanoid();
+		const task = search;
+		const checked = false;
+		const newTask = { id, checked, task };
+		const updatedTask = [...tasks, newTask];
+		setSearch("");
+		setTasks(updatedTask);
+	};
 
-    const addNewTask = () => {
-        const id = nanoid();
-        const task = search;
-        const newTask = {id, task};
-        const updatedTask = [...tasks, newTask];
-        setSearch('');
-        setTasks(updatedTask);
-    }
-
-    return (
-        <>
-            <div className="container">
-                <div id="to-do-heading">
-                    <h2>To-Do List</h2>
-                    <img src="Images/icon.png" alt="To-Do-List Image" />
-                </div>
-                <div id="search-bar">
-                    <input
-                        type="text"
-                        name="SearchBar"
-                        id="inputField"
-                        required
-                        placeholder="Add your task"
-                        autoFocus
-                        spellCheck='false'
-                        value={search}
-                        onChange={(e)=>setSearch(e.target.value)}
-                    />
-                    <button
-                        id="button-img"
-                        onClick={addNewTask}
-                    >Add</button>
-                </div>
-                <div>
-                    {tasks.length ? (
-                        tasks.map((item) => (
-                            <Tasks
-                                tasks={tasks}
-                                setTasks={setTasks}
-                                item={item}
-                                key={item.id}
-                            />
-                        ))
-                    ) : <p style={{
-                        color: "red",
-                        textAlign: "left",
-                        paddingLeft: "22px",
-                        paddingBottom: "5px"
-                    }}
-                    >Nothing to display!</p>}
-
-                </div>
-            </div >
-        </>
-    )
-}
-export default addTask
+	return (
+		<>
+			<div className='container'>
+				<div id='to-do-heading'>
+					<h2>To-Do List</h2>
+					<img src='Images/icon.png' alt='To-Do-List Image' />
+				</div>
+				<div id='search-bar'>
+					<input
+						type='text'
+						name='SearchBar'
+						id='inputField'
+						required
+						placeholder='Add your task'
+						autoFocus
+						spellCheck='false'
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+					<button id='button-img' onClick={search && addNewTask}>
+						Add
+					</button>
+				</div>
+				<div>
+					{tasks.length ? (
+						tasks.map((item) => (
+							<Tasks
+								tasks={tasks}
+								setTasks={setTasks}
+								item={item}
+								key={item.id}
+							/>
+						))
+					) : (
+						<p
+							style={{
+								color: "red",
+								textAlign: "left",
+								paddingLeft: "22px",
+								paddingBottom: "5px",
+							}}
+						>
+							Nothing to display!
+						</p>
+					)}
+				</div>
+			</div>
+		</>
+	);
+};
+export default addTask;
