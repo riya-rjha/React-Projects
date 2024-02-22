ef2d5d6af9564cfd4ee5b1c4596dc9631681b423
riya-rjha <riyaranjanjha2006@gmail.com>
2024-02-22 11:31:18 +0530

    Finalizing Drag & Drop

--- DragDrop/src/App.jsx
+++ DragDrop/src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 function App() {
   const [item, setItem] = useState(null);
 
-  const handleStart = (item) => {
-    setItem(item);
+  const handleStart = (e, id) => {
+    setItem(document.getElementById(id));
   };
 
   const handleDrag = (e) => {
@@ -34,60 +33,68 @@ function App() {
     <div className="container">
       <div id="left">
         <div
+          id='list1'
           draggable
           className="list"
-          onDragStart={() => handleStart(document.getElementById("list1"))}
+          onDragStart={(e) => handleStart(e, "list1")}
           onDragOver={handleDrag}
           onDrop={(e) => handleDrop(e, 'left')}
         >
-          List No 1
+          <span role="img" aria-label="List Icon">📋</span> <div className='heading'> Reactjs - Components</div>
+          <p>Build reusable UI components with React.js</p>
         </div>
         <div
           id="list2"
           draggable
           className="list"
-          onDragStart={() => handleStart(document.getElementById("list2"))}
+          onDragStart={(e) => handleStart(e, "list2")}
           onDragOver={handleDrag}
           onDrop={(e) => handleDrop(e, 'left')}
         >
-          List No 2
+          <span role="img" aria-label="List Icon">📋</span><div className='heading'> Nextjs - Animations</div>
+          <p>Create stunning animations with Next.js</p>
         </div>
         <div
           id="list3"
           draggable
           className="list"
-          onDragStart={() => handleStart(document.getElementById("list3"))}
+          onDragStart={(e) => handleStart(e, "list3")}
           onDragOver={handleDrag}
           onDrop={(e) => handleDrop(e, 'left')}
         >
-          List No 3
+          <span role="img" aria-label="List Icon">📋</span> <div className='heading'>HTML - Layouts</div>
+          <p>Design responsive layouts using HTML</p>
         </div>
         <div
           id="list4"
           draggable
           className="list"
-          onDragStart={() => handleStart(document.getElementById("list4"))}
+          onDragStart={(e) => handleStart(e, "list4")}
           onDragOver={handleDrag}
           onDrop={(e) => handleDrop(e, 'left')}
         >
-          List No 4
+          <span role="img" aria-label="List Icon">📋</span> <div className='heading'>CSS - Styling</div>
+          <p>Add beautiful styles to your web pages with CSS</p>
         </div>
         <div
           id="list5"
           draggable
           className="list"
-          onDragStart={() => handleStart(document.getElementById("list5"))}
+          onDragStart={(e) => handleStart(e, "list5")}
           onDragOver={handleDrag}
           onDrop={(e) => handleDrop(e, 'left')}
         >
-          List No 5
+          <span role="img" aria-label="List Icon">📋</span> <div className='heading'>JavaScript - Functionalities</div>
+          <p>Implement various functionalities using JavaScript</p>
         </div>
       </div>
       <div
         id="right"
         onDragOver={handleDrag}
         onDrop={(e) => handleDrop(e, 'right')}
-      ></div>
+      >
+        <p>Drop In Here If Completed</p>
+      </div>
     </div>
   );
 }
