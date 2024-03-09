0ace759e30abc7972f2f191b5f413ec24f90aad4
riya-rjha <riyaranjanjha2006@gmail.com>
2024-03-09 20:56:09 +0530

    Updated title and add progress bar functionality

--- Progress Bar/src/App.jsx
+++ Progress Bar/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div>Application</div>
-  )
-}
+    <div className='flex items-center justify-center flex-col mt-24 w-full gap-14'>
+      <div className="flex items-center justify-around gap-16">
+        <p className='text-2xl'>Click for Success!</p>
+        <button className='outline-none p-2 rounded-lg bg-red-100 border-4 font-semibold'>Success</button>
+      </div>
+      <div className="flex items-center justify-around gap-16">
+        <p className='text-2xl'>Click for Error!</p>
+        <button className='outline-none p-2 rounded-lg bg-red-100 border-4 font-semibold'>Error</button>
+      </div>
+      <div className="flex items-center justify-around gap-16">
+        <p className='text-2xl'>Click for Invalid!</p>
+        <button className='outline-none p-2 rounded-lg bg-red-100 border-4 font-semibold'>Invalid</button>
+      </div>
+    </div>
+  );
+};
 
-export default App  
+export default App;
